e48c6baca05a1c9b07cc9485b218f002a3fbc36c
Colby Eagan <colbyeagan6@gmail.com>
2024-12-01 21:04:03 -0500

    created seperate apt and rating tables

--- main.py
+++ main.py
@@ -11,28 +11,41 @@ class ApartmentRating(Base):
 
     rating_id = Column(Integer, primary_key=True, autoincrement=True)
     rating_value = Column(Float, nullable=False)
-    apartment_id = Column(Integer, nullable=False)
+    apartment_id = Column(Integer, ForeignKey('apartments.apartment_id'), nullable=False)
     user_id = Column(Integer, nullable=False)
     rent = Column(Float, nullable=False)
-    location = Column(String(255), nullable=False)
     bedrooms = Column(Integer, nullable=False)
     bathrooms = Column(Integer, nullable=False)
-    amenities = Column(Text, nullable=True)
-    year = Column(Integer, nullable=True)
+    year_of_review = Column(Integer, nullable=True)
 
-    def __init__(self, rating_value, apartment_id, user_id, rent, location, bedrooms, bathrooms, amenities, year):
+    def __init__(self, rating_value, apartment_id, user_id, rent, bedrooms, bathrooms, year_of_review):
         self.rating_value = rating_value
         self.apartment_id = apartment_id
         self.user_id = user_id
         self.rent = rent
-        self.location = location
         self.bedrooms = bedrooms
         self.bathrooms = bathrooms
+        self.year_of_review = year_of_review
+
+class Apartments(Base):
+    __tablename__ = 'apartments'
+
+    overall_rating_value = Column(Integer, nullable=False)
+    apartment_id = Column(Integer, nullable=False, primary_key=True)
+    location = Column(String(255), nullable=False)
+    amenities = Column(Text, nullable=True)
+    year_of_construction = Column(Integer, nullable=True)
+
+    def __init__(self, overall_rating_value, apartment_id, location, amenities, year_of_construction):
+        self.overall_rating_value = overall_rating_value
+        self.apartment_id = apartment_id
+        self.location = location
         self.amenities = amenities
-        self.year = year
+        self.year_of_construction = year_of_construction
 
 # Function to initialize db
 def init_db():
+    from __main__ import ApartmentRating, Apartments
     engine = create_engine('sqlite:///UNCRentdb.db', echo=True)
     Base.metadata.create_all(engine)
     return engine
